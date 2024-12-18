b9ce2c57ff8a311170fe86ab1c557ae27a909a39
baripembo <erikawei@gmail.com>
2024-12-18 17:03:55 -0500

    fixing text in intro

--- src/js/viz.js
+++ src/js/viz.js
@@ -1,16 +1,8 @@
 $( document ).ready(function() {
-  //const DATA_PATH = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnZMN1guJCB44f-O6iP-JpNum4NJdL5Op5GEbrkAayk_V19UkmO56YzQ2vSsfVCVWl5eyOT-Yhh4Y-/pub?single=true&output=csv&gid=';
-
-  //const DATA_PATH = 'https://proxy.hxlstandard.org/api/data-preview.csv?url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vRguxePjzXGhVXDTL6-JuS5Vppx7fKnk-CBheunS_5RGDKV36tOfLHa5RZ94oO2pDCLcdNC8BBisJzT%2Fpub%3Foutput%3Dcsv%26gid%3D'
   const DATA_PATH = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRguxePjzXGhVXDTL6-JuS5Vppx7fKnk-CBheunS_5RGDKV36tOfLHa5RZ94oO2pDCLcdNC8BBisJzT/pub?single=true&output=csv&gid=';
   const DATA_ID = 1103779481;
   const DATASET_COUNTS_ID = 733089483;
   const GLOBAL_COUNTS_ID = 2045883069;
-  // const DATA_PATH = 'https://proxy.hxlstandard.org/api/data-preview.csv?url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTnZMN1guJCB44f-O6iP-JpNum4NJdL5Op5GEbrkAayk_V19UkmO56YzQ2vSsfVCVWl5eyOT-Yhh4Y-%2Fpub%3Foutput%3Dcsv%26gid%3D'
-  // const DATA_ID = 1103779481;
-  // const DATASET_COUNTS_ID = 733089483;
-  // const GLOBAL_COUNTS_ID = 2045883069;
-  // const COUNTRIES_ID = 735983640;
 
   var isMobile = $(window).width()<=768 ? true : false;
   var countryCount, categoryCount, globalCounts, date;
